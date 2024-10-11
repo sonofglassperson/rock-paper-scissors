47895db3250970d9877fb96220be317a6877e98b
sonofglassperson <ntshabalalait@gmail.com>
2024-10-11 22:48:36 +0200

    Create getHumanChoice function

--- script.js
+++ script.js
@@ -4,14 +4,9 @@ function random(max) {
     return Math.floor((Math.random() * max) + 1)
 }
 
-
-function getComputerChoice(params) {
-   
-    let rand = random(3)
-    console.log(rand);
+function getChoice(num) {
     let computerChoice = null
-    // return a object in later renditions {value:rock, index:0}
-    switch (rand) {
+    switch (num) {
         case 1:
             computerChoice = 'rock'
             break;
@@ -22,7 +17,43 @@ function getComputerChoice(params) {
             computerChoice = 'scissors'
             break;
     }
-    return computerChoice
+    return computerChoice;
 }
 
-console.log(getComputerChoice())
+
+function getComputerChoice() {
+    let rand = random(3)
+    // return a object in later renditions {value:rock, index:0}
+    return getChoice(rand)
+}
+
+function getHumanChoice() {
+    let humanChoice = "";
+    let gameOptions = ['rock', 'paper', 'scissors', '1', '2', '3'];
+    
+    // read while like if
+    // if the condition is true, run code till it is false
+    while (humanChoice === "") { // 
+        humanChoice = prompt('rock (1), paper (2) or scissors (3)?')
+        if (humanChoice === "" || (humanChoice != null 
+                && ! gameOptions.includes(humanChoice.toLowerCase()))
+        ) {
+            alert("please select a valid choice!"); // Alert if input is empty
+            humanChoice = "";
+        } else {
+            // humanChoice isn't null & humanChoice isn't a Number
+            if (humanChoice != null && ! Number.isInteger(+humanChoice)) {
+                return humanChoice.toLowerCase()
+            } else if (humanChoice != null) {
+                return getChoice(+humanChoice); 
+            }
+        }
+    }
+
+    return 'No choice selected!';
+}
+
+// Test code here:
+console.log(getHumanChoice())
+
+//getHumanChoice()
